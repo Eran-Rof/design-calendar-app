13cd3a5284834da7b5e9f980d592f29d90495fac
Eran-Rof <eran@ringoffireclothing.com>
2026-03-17 18:11:21 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import TandAApp from './TandA.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const path = window.location.pathname;
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  path.startsWith('/tanda') ? <TandAApp /> : <App />
 )
